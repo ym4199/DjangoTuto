aae64bc07ba7c7c0853748391999f22c86073d25
ym4199 <ym4199@gmail.com>
2017-06-02 15:36:43 +0900

    add vote count

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -37,7 +37,11 @@ def detail(request, question_id):
 
 def results(request, question_id):
     response = "youre looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context={
+        'question':question,
+    }
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
